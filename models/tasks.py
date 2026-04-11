fcaad1f708f2802c863baf0972feb412a0da3d22
PlutonAleksandr <kotara13456@gmail.com>
2026-04-11 14:41:02 +0500

    Исправлен приоритет у задач

--- models/tasks.py
+++ models/tasks.py
@@ -8,9 +8,9 @@ from models.statuses import TaskStatus
 class Tasks(Base):
     __tablename__ = "tasks"
 
-
     id: Mapped[intpk]
     title: Mapped[str_256]
+    priority: Mapped[int] # через pydantic проверять что в диапозоне от 1 до 10
     description: Mapped[str] = mapped_column(nullable=False)
     status: Mapped[TaskStatus] = mapped_column(default=TaskStatus.not_issued, nullable=False)
 
